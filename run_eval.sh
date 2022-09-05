72f437b836ec78586f8690ea8eeca7a1c7e6150b
Lisa Wimmer <wml1293@outlook.de>
2022-09-05 12:47:08 +0200

    dynamic dir

--- run_eval.sh
+++ run_eval.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 LATESTRESULT=$(ls -td result/* | head -1)
-python scd_to_huggingface.py --path LATESTRESULT && \
+python scd_to_huggingface.py --path $LATESTRESULT && \
 python evaluation.py \
 --pooler cls_before_pooler \
 --task_set transfer \
 --mode test \
---model_name_or_path LATESTRESULT \
+--model_name_or_path $LATESTRESULT \
 --logfile logfile.json
 
