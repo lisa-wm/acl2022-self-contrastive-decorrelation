3830689eaa3ba715187dae93990c2080330f7307
Lisa Wimmer <wml1293@outlook.de>
2022-09-05 12:46:36 +0200

    dynamic dir

--- run_eval.sh
+++ run_eval.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
-python scd_to_huggingface.py --path result/hardy-lion-21 && \
+LATESTRESULT=$(ls -td result/* | head -1)
+python scd_to_huggingface.py --path LATESTRESULT && \
 python evaluation.py \
 --pooler cls_before_pooler \
 --task_set transfer \
 --mode test \
---model_name_or_path result/hardy-lion-21 \
+--model_name_or_path LATESTRESULT \
 --logfile logfile.json
 
