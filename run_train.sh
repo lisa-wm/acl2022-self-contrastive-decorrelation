0d270183e0c895eb70595d8be97d43724ace647e
Lisa Wimmer <wml1293@outlook.de>
2022-09-07 08:25:31 +0200

    set training config to original from paper

--- run_train.sh
+++ run_train.sh
@@ -5,21 +5,19 @@ python train.py \
 --load_best_model_at_end \
 --overwrite_output_dir \
 --description=SCD \
---eval_steps=2 \
+--eval_steps=250 \
 --evaluation_strategy=steps \
+--fp-16\
 --hidden_dropout_prob=0.05 \
 --hidden_dropout_prob_noise=0.155 \
---alpha_unc=0.01 \
---lambda_unc=10.0 \
 --learning_rate=3e-05 \
 --max_seq_length=32 \
---max_steps=5 \
 --metric_for_best_model=sickr_spearman \
 --model_name_or_path=bert-base-uncased \
---n_projectors=2 \
+--n_projectors=1 \
 --num_train_epochs=5 \
 --output_dir=result \
---per_device_train_batch_size=96 \
+--per_device_train_batch_size=192 \
 --report_to=wandb \
 --save_total_limit=0 \
 --task_alpha=1 \
@@ -27,4 +25,6 @@ python train.py \
 --task_lambda=0.012 \
 --temp=0.05 \
 --train_file=data/wiki1m_for_simcse.txt \
+--alpha_unc=0.01 \
+--lambda_unc=10.0 \
 
