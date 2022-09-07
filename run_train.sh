957f75839d373b1c5c0cc495e06387c4b89a17f4
lisa-wm <wml1293@outlook.de>
2022-09-07 16:30:07 +0200

    full run with our loss and 2 heads

--- run_train.sh
+++ run_train.sh
@@ -14,7 +14,7 @@ python train.py \
 --max_seq_length=32 \
 --metric_for_best_model=sickr_spearman \
 --model_name_or_path=bert-base-uncased \
---n_projectors=1 \
+--n_projectors=2 \
 --num_train_epochs=5 \
 --output_dir=result \
 --per_device_train_batch_size=192 \
