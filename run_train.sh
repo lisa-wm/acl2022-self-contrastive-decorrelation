26129507595b4bb1ecabedcef27570e88b80a110
lisa-wm <wml1293@outlook.de>
2022-09-14 12:50:16 +0200

    ran with 2 epochs

--- run_train.sh
+++ run_train.sh
@@ -15,7 +15,7 @@ python train.py \
 --metric_for_best_model=sickr_spearman \
 --model_name_or_path=bert-base-uncased \
 --n_projectors=5 \
---num_train_epochs=1 \
+--num_train_epochs=2 \
 --output_dir=result \
 --per_device_train_batch_size=96 \
 --report_to=wandb \
