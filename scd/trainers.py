67baa80b371b31d06bc6d14e18e97a2c011e42d9
Lisa Wimmer <wml1293@outlook.de>
2022-09-15 06:53:27 +0200

    change logging

--- scd/trainers.py
+++ scd/trainers.py
@@ -63,6 +63,7 @@ from torch.utils.data.dataset import Dataset
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data.sampler import RandomSampler, SequentialSampler
 from SentEval.senteval.engine import SE
+import SentEval.senteval_lisa.engine as se_lisa
 
 if is_torch_tpu_available():
     import torch_xla.core.xla_model as xm
@@ -149,6 +150,7 @@ class CLTrainer(Trainer):
             tasks = ['STSBenchmark', 'SICKRelatedness', 'MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']
         self.model.eval()
         results = se.eval(tasks)
+        breakpoint()
         
         stsb_spearman = results['STSBenchmark']['dev']['spearman'][0]
         sickr_spearman = results['SICKRelatedness']['dev']['spearman'][0]
